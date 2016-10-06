110bd0b2554e48e5f8ed18ad3afccd05c7f21c03
Sandilya Jandhyala <sandy@nilenso.com>
2016-10-06 11:38:36 +0530

    Configure ragtime for migrations

--- src/time_tracker/config.clj
+++ src/time_tracker/config.clj
@@ -1,7 +1,14 @@
 (ns time-tracker.config
   (:require [clojure.java.io :as io]
-            [nomad :refer [defconfig]]))
+            [nomad :refer [defconfig]]
+            [ragtime.jdbc]
+            [ragtime.repl]))
 
 (defconfig app-config (io/file "config/backend-config.edn"))
 
 (def google-tokeninfo-url (:google-tokeninfo-url (app-config)))
+
+(def migration-config
+  {:datastore  (ragtime.jdbc/sql-database {:connection-uri (:db-connection-string (app-config))})
+   :migrations (ragtime.jdbc/load-resources (:migrations-resource-dir (app-config)))})
+
---
+++ src/time_tracker/migration.clj
@@ -0,0 +1,10 @@
+(ns time-tracker.migration
+  (:require [ragtime.repl]
+
+            [time-tracker.config :refer [migration-config]]))
+
+(defn migrate-db []
+  (ragtime.repl/migrate migration-config))
+
+(defn rollback-db []
+  (ragtime.repl/rollback migration-config))
