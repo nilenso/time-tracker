7f4b23e883a48edc82d8f3aa19f6aa043d9e6492
Sandilya Jandhyala <sandy@nilenso.com>
2016-10-17 12:39:30 +0530

    Add fakes and fixtures for functional tests

--- test/time_tracker/fixtures.clj
+++ test/time_tracker/fixtures.clj
@@ -1,17 +1,26 @@
 (ns time-tracker.fixtures
   (:require [clojure.java.jdbc :as jdbc]
-
             [time-tracker.migration :refer [migrate-db]]
-            [time-tracker.db :as db]))
+            [time-tracker.db :as db]
+            [time-tracker.core :refer [app]]
+            [time-tracker.auth.core :as auth]
+            [time-tracker.auth.test-helpers :refer [fake-token->credentials]])
+  (:use org.httpkit.server))
 
 (defn migrate-test-db [f]
-  (println "Migrating the databse")
   (migrate-db)
-  (println "The database has been migrated.")
   (f))
+
+(defn serve-app [f]
+  (with-redefs [auth/token->credentials
+                fake-token->credentials]
+    (let [stop-fn (run-server app {:port 8000})]
+      (f)
+      (stop-fn :timeout 100))))
 
 (defn isolate-db [f]
   (jdbc/with-db-transaction [conn (db/connection)]
     (jdbc/db-set-rollback-only! conn)
     (with-redefs [db/connection (fn [] conn)]
       (f))))
+
