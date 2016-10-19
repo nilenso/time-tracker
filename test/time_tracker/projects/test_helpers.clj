8ee03556901bb45c9ee936ab2fd20458decf0511
Sandilya Jandhyala <sandy@nilenso.com>
2016-10-19 17:01:15 +0530

    Refactor populate-projects!

--- test/time_tracker/projects/test_helpers.clj
+++ test/time_tracker/projects/test_helpers.clj
@@ -5,6 +5,39 @@
 
 (defqueries "time_tracker/projects/sql/db.sql")
 
+(defn- create-project-as-user!
+  "Creates a project and gives the user admin permissions.
+  Returns the project id."
+  [connection user-id project-name]
+  (let [{project-id :id} (first (jdbc/insert!
+                                 connection "project"
+                                 {"name" project-name}))]
+    (create-admin-permission-query! {:project_id project-id
+                                     :user_id    user-id}
+                                    {:connection connection})
+    project-id))
+
+(defn- create-user!
+  "Creates a user and returns the user id."
+  [connection google-id]
+  ;; Not using time-tracker.users.test-helpers.create-users!
+  ;; because that doesn't return the user id.
+  (:id (first
+        (jdbc/insert! connection "app_user"
+                      {"google_id" google-id
+                       "name" "Agent Smith"}))))
+
+(defn- populate-projects!
+  "Given a user, creates projects for the user
+  and grants admin permissions on those projects.
+  Returns {project-name project-id}"
+  [connection user-id project-names]
+  (->> (for [project-name project-names]
+         (let [project-id (create-project-as-user!
+                           connection user-id project-name)]
+           [project-name project-id]))
+       (into {})))
+
 (defn populate-data!
   "In: {google-id [list of owned projects]}
   Out: {project-name project-id} 
@@ -12,17 +45,8 @@
   [test-data]
   (jdbc/with-db-transaction [conn (db/connection)]
     (reduce (fn [project-name-ids [google-id project-names]]
-              (let [{user-id :id} (first (jdbc/insert! conn "app_user"
-                                                       {"google_id" google-id
-                                                        "name" "Agent Smith"}))]
+              (let [user-id (create-user! conn google-id)]
                 (merge project-name-ids
-                       (->> (for [project-name project-names]
-                              (let [{project-id :id} (first (jdbc/insert! conn "project"
-                                                                          {"name" project-name}))]
-                                (create-admin-permission-query! {:project_id project-id
-                                                                 :user_id    user-id}
-                                                                {:connection conn})
-                                [project-name project-id]))
-                            (into {})))))
+                       (populate-projects! conn user-id project-names))))
             {}
             test-data)))
