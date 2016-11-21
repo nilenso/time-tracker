38c7c98c13617ebafaa24e2e25db0cca1cb62479
Sandilya Jandhyala <sandy@nilenso.com>
2016-11-21 13:55:51 +0530

    Nuke useless files

--- src/time_tracker/users/handlers.clj
+++
@@ -1,9 +0,0 @@
-(ns time-tracker.users.handlers
-  (:require [ring.util.response :as res]
-            [time-tracker.auth.core :refer [wrap-auth]]))
-
-(def user-details
-  (-> (fn [request]
-        (res/response (:credentials request)))
-      (wrap-auth)))
-
