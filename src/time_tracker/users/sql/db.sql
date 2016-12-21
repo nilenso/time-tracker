2e34369a65e172491ac95d4b99ca084c489ef6bd
Sandilya Jandhyala <sandy@nilenso.com>
2016-12-21 11:27:43 +0530

    Add API to retrieve a user's own information

--- src/time_tracker/users/sql/db.sql
+++ src/time_tracker/users/sql/db.sql
@@ -1,7 +1,8 @@
--- name: is-registered-query
--- Check if the user is in the database.
+-- name: retrieve-user-data-query
+-- Retrieves user data.
 SELECT * FROM app_user
-WHERE app_user.google_id = :google_id;
+WHERE app_user.google_id = :google_id
+LIMIT 1;
 
 -- name: register-user-query!
 -- Registers a user in the database.
@@ -9,3 +10,4 @@ INSERT INTO app_user
 (google_id, name)
 VALUES (:google_id, :name)
 ON CONFLICT DO NOTHING;
+
