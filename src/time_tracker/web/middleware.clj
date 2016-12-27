75411058ec6b547f9793dceafa266bbe6ee9a1bd
Sandilya Jandhyala <sandy@nilenso.com>
2016-12-27 11:08:00 +0530

    Move middleware into time-tracker.web.middleware

--- src/time_tracker/web/middleware.clj
+++ src/time_tracker/web/middleware.clj
@@ -4,7 +4,9 @@
             [time-tracker.db :refer [wrap-transaction]]
             [time-tracker.users.core :refer [wrap-autoregister]]
             [time-tracker.util :as util]
-            [time-tracker.logging :as log]))
+            [time-tracker.logging :as log]
+            [cheshire.generate :refer [add-encoder encode-str]])
+    (:import org.httpkit.server.AsyncChannel))
 
 (def rest-middleware
   (comp wrap-auth wrap-transaction wrap-autoregister))
@@ -12,6 +14,42 @@
 (defn with-rest-middleware
   [routes-map]
   (fmap rest-middleware routes-map))
+
+;; This is neccessary because http-kit adds some AsyncChannel
+;; to the request map if it is a websockets connection upgrade,
+;; which can't be JSON serialized.
+(def standard-ring-request-keys
+  [:server-port :server-name :remote-addr :uri :query-string :scheme
+   :headers :request-method :body])
+
+;; Define a custom JSON encoder for the AsyncChannel
+(add-encoder AsyncChannel encode-str)
+
+(defn wrap-log-request-response
+  [handler]
+  (fn [request]
+    (let [response (handler request)]
+      (log/debug {:event    ::request-response
+                  :request  (select-keys request
+                                         standard-ring-request-keys)
+                  :response response})
+      response)))
+
+(defn wrap-error-logging
+  [handler]
+  (fn [request]
+    (try
+      (if-let [response (handler request)]
+        response
+        (do
+          (log/error {:event   ::nil-response
+                      :request request})
+          (util/error-response 500 "Internal Server Error")))
+      (catch Exception ex
+        (log/error ex {:event   ::unhandled-exception
+                       :request request})
+        (util/error-response 500 "Internal Server Error")))))
+
 
 (defn wrap-validate
   [handler]
