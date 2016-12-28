7908c887c2342fcddf5654a2b12f9ec19e63ef0b
Sandilya Jandhyala <sandy@nilenso.com>
2016-12-28 13:32:46 +0530

    Update the deploy script

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-USER=timetracker
-HOST=time.nilenso.com
-ssh $USER@$HOST 'touch test.foo'
+USER=timetracker;
+HOST=time.nilenso.com;
+ssh $USER@$HOST \
+    'source $HOME/env.sh; \
+     cd time-tracker; \
+     git checkout ci-setup && git pull; \
+     killall -9 -v java; \
+     ($HOME/lein trampoline run &);
+     exit'
