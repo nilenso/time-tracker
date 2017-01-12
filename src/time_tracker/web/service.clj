0ef637324dd9d7b5912b67020b3bb117ee6e1b8c
Sandilya Jandhyala <sandy@nilenso.com>
2017-01-12 11:02:38 +0530

    Move service init and teardown into time-tracker.core

--- src/time_tracker/web/service.clj
+++ src/time_tracker/web/service.clj
@@ -1,6 +1,5 @@
 (ns time-tracker.web.service
-  (:require [time-tracker.logging :as log]
-            [bidi.ring :refer [make-handler]]
+  (:require [bidi.ring :refer [make-handler]]
             [ring.middleware.json :refer [wrap-json-response
                                           wrap-json-body]]
             [ring.middleware.params :refer [wrap-params]]
@@ -8,10 +7,7 @@
             [time-tracker.web.routes :refer [routes]]
             [time-tracker.web.middleware :refer [wrap-validate
                                                  wrap-log-request-response
-                                                 wrap-error-logging]]
-            [time-tracker.db :as db]
-            [org.httpkit.server :as httpkit]
-            [time-tracker.util :as util]))
+                                                 wrap-error-logging]]))
 
 (defn handler []
   (make-handler (routes)))
@@ -26,34 +22,3 @@
       (wrap-params)
       (wrap-defaults api-defaults)))
 
-(defn init! []
-  (log/configure-logging!)
-  (db/init-db!))
-
-(defn teardown! []
-  (log/teardown-logging!))
-
-(defn start-server! []
-  (init!)
-  (log/info {:event ::server-start})
-  (let [stop-fn
-        (httpkit/run-server (app) {:port (Integer/parseInt (util/from-config :port))})]
-    (fn []
-      (stop-fn)
-      (log/info {:event ::server-stop})
-      (teardown!))))
-
-(defn start-server-as-gid!
-  "Starts the server and ensures that requests are always
-  authenticated as `google-id`. Useful for repl testing.
-  Returns a fuction to reset the var and stop the server."
-  [google-id name]
-  (let [reset-auth-var-fn (util/rebind-var!
-                           (var time-tracker.auth.core/auth-credentials)
-                           (constantly {:sub  google-id
-                                        :name name
-                                        :hd   "nilenso.com"}))
-        stop-server-fn    (start-server!)]
-    (fn []
-      (stop-server-fn)
-      (reset-auth-var-fn))))
