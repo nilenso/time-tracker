83c3fc6e8c89fb6865c7e77c3d2996347c7e6f1f
Harsh Gupta <harsh@nilenso.com>
2018-04-19 17:43:58 +0530

    Separate out specs for creating projects and modifying projects
    Co-authored-by: Samrat Man Singh <samrat@nilenso.com>

--- src/time_tracker/projects/spec.clj
+++ src/time_tracker/projects/spec.clj
@@ -6,9 +6,13 @@
 
 (s/def ::id ::core-spec/id)
 (s/def ::name ::core-spec/non-empty-string)
+(s/def ::client-id ::clients-spec/id)
 
 (s/def ::project
   (s/keys :req-un [::id ::name]))
 
-(s/def ::project-input
-  (s/keys :req-un [::name ::clients-spec/client-id]))
+(s/def ::project-create-input
+  (s/keys :req-un [::name ::client-id]))
+
+(s/def ::project-modify-input
+  (s/keys :req-un [::name]))
