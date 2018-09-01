70b83db0a878221827332d1e8630c2c7ca0a7fbd
Abhinandan Kothari <abhinandankothari@gmail.com>
2018-09-01 19:06:27 +0530

    [Shafeeq|Abhi][#189] Sends email when user is invited

--- project.clj
+++ project.clj
@@ -21,7 +21,8 @@
                  [ring/ring-defaults "0.2.1"]
                  [org.clojure/algo.generic "0.1.2"]
                  [clj-time "0.12.0"]
-                 [clj-pdf "2.2.29"]]
+                 [clj-pdf "2.2.29"]
+                 [nilenso/mailgun "0.2.3"]]
   :main ^:skip-aot time-tracker.core
   :target-path "target/%s"
   :plugins [[lein-environ "1.1.0"]]
--- src/time_tracker/invited_users/handlers.clj
+++ src/time_tracker/invited_users/handlers.clj
@@ -2,7 +2,20 @@
   (:require [time-tracker.invited-users.db :as invited-users-db]
             [time-tracker.users.db :as users-db]
             [time-tracker.web.util :as web-util]
-            [ring.util.response :as res]))
+            [ring.util.response :as res]
+            [time-tracker.util :refer [from-config] :as util]
+            [mailgun.mail :as mail]
+            [time-tracker.logging :as log]))
+
+(defn send-email
+  [email]
+  (let [creds {:key (from-config :mailgun-key) :domain (from-config :mailgun-domain)}
+        content {:from    "admin@time.nilenso.com"
+                 :to      email
+                 :subject "You have been invited to Join Time Tracker"
+                 :html    "Welcome, Visit https://time.nilenso.com and Sign In with this email"}]
+    (mail/send-mail creds content)
+    (log/info "Email Sent Successfully Via Mail Gun")))
 
 (defn create
   [{:keys [body credentials] :as request} connection]
@@ -11,6 +24,7 @@
         invited-by (users-db/retrieve connection google-id)]
     (if (users-db/has-user-role? google-id connection ["admin"])
       (let [invited-user (invited-users-db/create! connection email (:id invited-by))]
+        (send-email email)
         (res/response invited-user))
       web-util/error-forbidden)))
 
