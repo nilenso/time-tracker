34a43e93db8daa2ed4e38a472500248daab296bf
Sandilya Jandhyala <jysandilya@gmail.com>
2020-08-12 12:26:05 +0530

    Integrate shadow-cljs with lein
    Now, dependencies are managed using lein only.
    This is required for Cursive to play nicely with
    the project.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha13"]
                  [org.clojure/tools.logging "0.3.1"]
                  [org.clojure/tools.cli "1.0.194"]
@@ -30,15 +30,24 @@
   :main ^:skip-aot time-tracker.core
   :target-path "target/%s"
   :plugins []
-  :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]
-                                  [org.clojure/core.async "0.2.395"]
-                                  [stylefruits/gniazdo "1.0.0"]]}
-             :test {:jvm-opts ["-Xms512m" "-Xmx2g"]}
+  :profiles {:dev     {:dependencies [[org.clojure/test.check "0.9.0"]
+                                      [org.clojure/core.async "1.3.610"]
+                                      [stylefruits/gniazdo "1.0.0"]]}
+             :test    {:jvm-opts ["-Xms512m" "-Xmx2g"]}
              :default [:base :system :user :provided :dev]
-             :uberjar {:aot [#"time-tracker.*"]}}
+             :uberjar {:aot [#"time-tracker.*"]}
+             :cljs    {:source-paths ["src/cljs" "test/cljs"]
+                       :dependencies [[re-frame "1.0.0"]
+                                      [day8.re-frame/http-fx "0.2.1"]
+
+                                      ;; dev dependencies
+                                      [org.clojure/clojure "1.10.1"] ; shadow-cljs requires clojure 1.10.1
+                                      [thheller/shadow-cljs "2.10.21"]
+                                      [day8.re-frame/re-frame-10x "0.7.0"]
+                                      [binaryage/devtools "1.0.2"]]}}
   :jvm-opts ["-Dclojure.tools.logging.factory=clojure.tools.logging.impl/log4j2-factory"]
-  :aliases {"test"       ["test"]
-            "migrate"    ["run" "-m" "time-tracker.migration/lein-migrate-db"]
-            "rollback"   ["run" "-m" "time-tracker.migration/lein-rollback-db"]}
+  :aliases {"test"     ["test"]
+            "migrate"  ["run" "-m" "time-tracker.migration/lein-migrate-db"]
+            "rollback" ["run" "-m" "time-tracker.migration/lein-rollback-db"]}
   :monkeypatch-clojure-test false
   :uberjar-exclusions [#"dev.*"])
